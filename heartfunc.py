9f47d33d9680a8fe1ccf30af409b506b2e1f03a2
SUGANYA-SENTHIL-KUMAR <162526216+SUGANYA-SENTHIL-KUMAR@users.noreply.github.com>
2025-03-12 23:51:49 +0530

    Add files via upload

---
+++ heartfunc.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def heart_silhouette():
+    m = np.linspace(0, 2 * np.pi, 1000)
+    
+    a = []
+    b = []
+    
+    for value in m:
+        a.append(16 * (np.sin(value) ** 3))
+        b.append(13 * np.cos(value) - 5 * np.cos(2 * value) - 2 * np.cos(3 * value) - np.cos(4 * value))
+    
+    plt.figure(figsize=(6, 6))
+    plt.plot(a, b, color='black', linewidth=3)
+    plt.axhline(0, color='gray', linewidth=0.5) 
+    plt.axvline(0, color='gray', linewidth=0.5) 
+    plt.xlabel("X-axis")  # Add x-axis label
+    plt.ylabel("Y-axis")  # Add y-axis label
+    plt.axis("equal")
+    plt.show()
+
+heart_silhouette()
